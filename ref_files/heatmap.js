2b3ab7b6629cae4589e1ec55dc065444acfdd9f4
Phyo Kyi <phyokyi1986@gmail.com>
2020-04-25 22:29:58 +0630

    Info panel added

--- ref_files/heatmap.js
+++ ref_files/heatmap.js
@@ -58,12 +58,15 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+
+    info.update(layer.feature.properties);
 }
 
 var geojson;
 
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
+    info.update();
 }
 
 function zoomToFeature(e) {
@@ -80,7 +83,7 @@ function onEachFeature(feature, layer) {
 
 function mapFilter(feature) {
     if (feature.properties["ST_PCODE"] != "") return true
-  }
+}
 
 geojson = L.geoJson(geoData, {
     style: style,
@@ -89,3 +92,38 @@ geojson = L.geoJson(geoData, {
 });
 
 geojson.addTo(map);
+
+
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+function get_value(_pcode){
+    var result = ranks.filter(obj => {
+        return obj.pcode === _pcode
+    });
+    return result[0].value;
+}
+function get_rank(_pcode){
+    var result = ranks.filter(obj => {
+        return obj.pcode === _pcode
+    });
+    return result[0].rank;
+}
+info.update = function (props) {
+    var v = props;
+    console.log(v);
+    this._div.innerHTML = "<h4>Excel Heat Mapping</h4>"+(props ? 
+        "State/Region " + props.ST + "<br>" +
+        "State/Region PCode : " + props.ST_PCODE + "<br>" +
+        "Value : " + get_value(props.ST_PCODE) + "<br>" +
+        "Rank : " + get_rank(props.ST_PCODE) + "<br>"
+        : "Hover over a state");
+};
+
+info.addTo(map);
